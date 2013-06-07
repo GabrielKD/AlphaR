3ccc37e1c9ffa0decc82296980bcab4a05191d5b
Dan Gabriel Diaconescu <ddiac1st@gmail.com>
2013-06-08 00:58:39 +0300

    - modified mouse look script to support 2 mouse look styles;

--- Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
@@ -29,28 +29,55 @@ public class MouseLook : MonoBehaviour {
 	public float maximumY = 60F;
 
 	float rotationY = 0F;
+	
+	// Mouse Look Style
+	// 0 - MMO style 0 - default no mouse look; mouse look on right click
+	// 1 - MMO style 1 - default mouse look; interrupt/resume mouse look on certain keys (default: Left Alt & Escape)
+	// Will be read from PlayerPrefs
+	private int mouseLookStyle = 1;
+	
+	// Mouse view flag - false if enabled;
+	private bool mouseViewActive = true;
 
 	void Update ()
 	{
-		if (axes == RotationAxes.MouseXAndY)
+		establishMouseView();
+		
+		Debug.Log (mouseViewActive);
+		if( mouseViewActive == true )
 		{
-			float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+			if( Screen.showCursor == true )
+			{
+				Screen.showCursor = false;
+			}
 			
-			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-			
-			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-		}
-		else if (axes == RotationAxes.MouseX)
-		{
-			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+			if (axes == RotationAxes.MouseXAndY)
+			{
+				float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+				
+				rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+				rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+				
+				transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+			}
+			else if (axes == RotationAxes.MouseX)
+			{
+				transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+			}
+			else
+			{
+				rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+				rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+				
+				transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+			}
 		}
 		else
 		{
-			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-			
-			transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+			if( Screen.showCursor == false )
+			{
+				Screen.showCursor = true;
+			}
 		}
 	}
 	
@@ -59,5 +86,40 @@ public class MouseLook : MonoBehaviour {
 		// Make the rigid body not change rotation
 		if (rigidbody)
 			rigidbody.freezeRotation = true;
+	}
+	
+	private void establishMouseView()
+	{
+		switch(mouseLookStyle)
+		{
+			case 0:
+				// Mouse look via right click
+				if( Input.GetMouseButtonDown(1) )
+				{
+					mouseViewActive = true;
+				}
+				
+				if( Input.GetMouseButtonUp(1) )
+				{
+					mouseViewActive = false;
+				}
+			break;
+			
+			case 1:
+				// Mouse look always on, disabled by specific keys
+				// TO DO: make keys custamisable
+				if( Input.GetKeyDown(KeyCode.LeftAlt) || Input.GetKeyDown(KeyCode.Escape) )
+				{
+					if( mouseViewActive == true )
+					{
+						mouseViewActive = false;
+					}
+					else
+					{
+						mouseViewActive = true;
+					}
+				}
+			break;
+		}
 	}
 }
